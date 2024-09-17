3a8655265136b53345fa1752e171532f1bc09335
zyad87 <zyadt871@gmail.com>
2024-09-17 08:14:56 +0300

    first commit

---
+++ home.js
@@ -0,0 +1,81 @@
+let userName = document.getElementById('userNameHeader');
+let cardsSection = document.getElementById('cardsSection');
+let signOut = document.getElementById('signOut');
+let bgAccess = document.getElementById('bgAccess');
+
+
+if (localStorage.getItem('userEmail') && localStorage.getItem('userName')) {
+  userName.innerText = `Welcome: `;
+  let userNameText = document.createElement('b');
+  userNameText.innerText = localStorage.getItem('userName');
+  userNameText.style.color = 'red';
+  userName.append(userNameText);
+  fetchMovies();
+} else {
+  let flexSection = document.createElement('div');
+  flexSection.classList.add('flexSection');
+  let textAuthorized = document.createElement('h1');
+  textAuthorized.innerText = 'Access not authorized';
+  textAuthorized.classList.add('text-danger', 'bg-light-subtle');
+  textAuthorized.style.padding = '1rem';
+  textAuthorized.style.borderRadius = '10px';
+  flexSection.appendChild(textAuthorized);
+  cardsSection.appendChild(flexSection);
+  signOut.innerHTML= "Back to login"
+  bgAccess.style.backgroundColor ="black"
+  bgAccess.style.height ="100vh"
+  userName.innerText = `Not logged in :(`;
+  
+}
+
+async function fetchMovies() {
+    let url = 'https://api.themoviedb.org/3/movie/popular?api_key=802d1355530b8a3d8cddc201b7e66441&language=en-US&page=1';
+  
+    let res = await fetch(url, { method: 'GET' });
+    let data = await res.json();
+    
+    data.results.forEach((element) => {
+      let cardContainer = document.createElement('div');
+      cardContainer.classList.add('col-12', 'col-md-3', 'mb-4', 'd-flex', 'align-items-stretch'); 
+  
+      let card = document.createElement('div');
+      card.classList.add('card', 'h-100'); 
+  
+      let img = document.createElement('img');
+      img.classList.add('card-img-top');
+      img.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`;
+      img.alt = element.title;
+  
+      let cardBody = document.createElement('div');
+      cardBody.classList.add('card-body', 'd-flex', 'flex-column');
+  
+      let title = document.createElement('h5');
+      title.classList.add('card-title');
+      title.innerText = element.title;
+  
+      let rating = document.createElement('p');
+      rating.classList.add('card-text');
+      rating.innerHTML = ` ${element.vote_average} <i class="fa-solid fa-star" style="color: gold;"></i>`;
+  
+      // إضافة زر "Watch Now"
+      let watchButton = document.createElement('a');
+      watchButton.classList.add('btn', 'btn-danger', 'mt-auto'); 
+      watchButton.href = '#'; 
+      watchButton.innerText = 'Watch Now';
+  
+      cardBody.append(title);
+      cardBody.append(rating);
+      cardBody.append(watchButton); 
+      card.append(img);
+      card.append(cardBody);
+      cardContainer.append(card);
+      cardsSection.append(cardContainer);
+    });
+  }
+  
+
+
+signOut.addEventListener('click', () => {
+  localStorage.clear();
+  window.location.href = './login.html';
+});
---
+++ min.js
@@ -0,0 +1,108 @@
+let url = `https://66e88c0bb17821a9d9dd0e96.mockapi.io/Login`;
+
+async function signupHandler(event) {
+  event.preventDefault();
+
+  let userName = document.getElementById('userName');
+  let userEmail = document.getElementById('userEmail');
+  let userPassword = document.getElementById('userPassword');
+  let userNameAlert = document.getElementById('userNameAlert');
+  let emailAlert = document.getElementById('emailAlert');
+  let passwordAlert = document.getElementById('passwordAlert');
+
+  userNameAlert.innerText = '';
+  emailAlert.innerText = '';
+  passwordAlert.innerText = '';
+
+  if (userName.value.length < 5) {
+    userNameAlert.innerText = 'Name should be at least 5 characters';
+    userNameAlert.style.color = 'red';
+  }
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.test(userEmail.value)) {
+    emailAlert.innerText = 'Email should be valid';
+    emailAlert.style.color = 'red';
+  }
+
+  if (userPassword.value.length < 8) {
+    passwordAlert.innerText = 'Password should be at least 8 characters';
+    passwordAlert.style.color = 'red';
+  }
+
+  if (
+    userName.value.length >= 5 &&
+    emailPattern.test(userEmail.value) &&
+    userPassword.value.length >= 8
+  ) {
+    await addUser(userName.value, userEmail.value, userPassword.value);
+    userName.value = '';
+    userEmail.value = '';
+    userPassword.value = '';
+
+    window.location.href = './login.html';
+  }
+}
+
+async function addUser(userName, userEmail, userPassword) {
+  let res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      userName: userName,
+      userEmail: userEmail,
+      userPassword: userPassword,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  let data = await res.json();
+  console.log(data);
+}
+
+
+async function loginHandler(event) {
+    event.preventDefault();
+  
+    let userEmail = document.getElementById('userEmail');
+    let userPassword = document.getElementById('userPassword');
+    let emailAlert = document.getElementById('emailAlert');
+    let passwordAlert = document.getElementById('passwordAlert');
+  
+    emailAlert.innerText = '';
+    passwordAlert.innerText = '';
+  
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(userEmail.value)) {
+      emailAlert.innerText = 'Email should be valid';
+      emailAlert.style.color = 'red';
+    }
+    if (userPassword.value.length < 8) {
+      passwordAlert.innerText = 'Password should be at least 8 characters';
+      passwordAlert.style.color = 'red';
+    }
+  
+    if (emailPattern.test(userEmail.value) && userPassword.value.length >= 8) {
+      let res = await fetch(url, { method: 'GET' });
+      let data = await res.json();
+      const userFound = data.find(
+        (user) =>
+          user.userEmail === userEmail.value &&
+          user.userPassword === userPassword.value
+      );
+      if (userFound) {
+        localStorage.setItem('userId', userFound.id);
+        localStorage.setItem('userEmail', userFound.userEmail);
+        localStorage.setItem('userName', userFound.userName);
+  
+        userEmail.value = '';
+        userPassword.value = '';
+        window.location.href = './home.html';
+      } else {
+        emailAlert.innerText = 'Email not found';
+        passwordAlert.innerText = 'Password does not match';
+        passwordAlert.style.color = 'red';
+        emailAlert.style.color = 'red';
+      }
+    }
+  }
